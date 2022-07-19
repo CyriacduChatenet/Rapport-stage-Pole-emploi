8cd65bcd8996b30a2bda762c72b35d84d4b98e09
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-07-19 11:16:26 +0200

    add custom fonts

---
+++ client/src/views/home/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Button extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>
