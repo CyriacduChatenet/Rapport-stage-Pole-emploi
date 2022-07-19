f3ec4ed3d9e3231f5419a5e3be4c33725b657121
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-07-19 10:54:51 +0200

    feat(app) : regenerate front

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,6 @@
-import { createApp, Vue } from 'vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHatWizard } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import App from './layout/App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-
-library.add(faHatWizard);
-
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).mount("#app");
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,27 +1,29 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import HomeView from '../views/home/HomeView.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: HomeView,
   },
   {
-    path: '/contact',
-    name: 'contact',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "contact" */ '../views/contact/ContactView.vue'),
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/contact",
+    name: "contact",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/about/AboutView.vue'),
+    component: () =>
+      import(/* webpackChunkName: "contact" */ "../views/ContactView.vue"),
   },
 ];
 
