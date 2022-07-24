01b11148b69e6596718ece9fa3e9c56fd8d49ae4
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-07-24 03:50:44 +0200

    feat(app) : add contact page

--- client/src/pages/contact/contact.tsx
+++ client/src/pages/contact/contact.tsx
@@ -1,7 +1,19 @@
+import { Layout } from "../../common/components/layout/layout";
+import "./contact.css";
+
 export const ContactPage = () => {
-    return (
-        <>
-            <h1>Hello react</h1>
-        </>
-    )
-}
+  const email = "cyriacduchatenet@icloud.com";
+  return (
+    <Layout>
+      <div id="contact">
+        <h1 className="contactTitle">Contact</h1>
+        <p
+          className="contactLink"
+          onClick={() => navigator.clipboard.writeText(email)}
+        >
+          cyriacduchatenet@icloud.com
+        </p>
+      </div>
+    </Layout>
+  );
+};
